9d2438a85c6ffe274598c078216ae7c7476f256d
KayoLino <kayofelipelino2002@gmail.com>
2024-08-29 09:51:27 -0300

    Adding game stages

---
+++ src/components/GameOver.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './GameOver.css'
+
+function GameOver({ retry }) {
+  return (
+    <div>
+      <h1>Game Over</h1>
+      <button onClick={retry}>Resetar jogo</button>
+
+    </div>
+  )
+}
+
+export default GameOver
